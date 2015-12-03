b353fccb06089468c34a806f0cde2b28b4d1c8ab
ajahraus <adamjahraus@gmail.com>
2015-12-03 16:30:00 -0700

    Updates to Plane function

--- Plane.m
+++ Plane.m
@@ -19,14 +19,32 @@ classdef Plane
                 obj.centroid = mean(corners);
                 
                 obj.distance_to_origin = abs(dot(obj.normal, obj.vertexs(1,:)));
-            
+                
             end
         end
         
-        function points = obj.samplePlane(numPoints)
+        
+        function points = samplePlane(obj, numPoints)
             % Needs some improvement
-            points = zeros(size(numPoints),3);
+            points = zeros(numPoints,3);
+            v = obj.vertexs;
+            
+            L1 = norm(v(2,:) - v(1,:));
+            L2 = norm(v(3,:) - v(1,:));
+            L3 = norm(v(3,:) - v(2,:));
+            
+            temp = sortrows([L1, v(2,:)-v(1,:),12; L2, v(3,:) - v(1,:),13; L3, v(3,:) - v(2,:),23]);
+            
+            Longest_vector = temp(3, 2:4);
+            Second_vector = temp(2, 2:4);
+            
+            
+            
         end
+    end
+    
+    methods (Static)
+        
     end
     
 end
