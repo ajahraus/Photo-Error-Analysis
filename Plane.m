45e95c58ca4f079ad54e20c2cdd927ed782f71ce
ajahraus <adamjahraus@gmail.com>
2015-12-08 13:50:41 -0700

    Completed Plane Sampling Algorithm

--- Plane.m
+++ Plane.m
@@ -27,31 +27,93 @@ classdef Plane
         
         
         function points = samplePlane(obj, numPoints)
-            % Needs some improvement
-            points = zeros(size(numPoints),3);
-            
-            point_spacing = sqrt(numPoints/obj.area);
-            v = obj.vertexs;
-            v1 = v(1,:);
-            v2 = v(2,:);
-            v3 = v(3,:);
+            % points = obj.samplePlane(numPoints)
+            % This function takes an argument of the number of points that
+            % the plane is to have sampled from it. The sampling is done in
+            % a grid pattern, evenly spaced along the largest axis and
+            % sampled orthogonally to it. The number of points in the
+            % sample is not exact, so the actual number of points returned
+            % may nat exactly correspond to the number of points specified
+            % in the input. 
+                        
+            point_spacing = sqrt(obj.area/numPoints);
+            vert = obj.vertexs;
+            v1 = vert(1,:);
+            v2 = vert(2,:);
+            v3 = vert(3,:);
             
             [~, index] = max( [norm(v2-v1),norm(v3-v1),norm(v3-v2)]);
             if index==1
                 base = v1;
-                dir = (v2-v1)/norm(v2-v1);
-                cross_dir = cross(obj.normal,dir);
+                vec1 = v2-base;
+                vec2 = v3-base;
             elseif index == 2
-                base = v1;
-                dir = (v3-v1)/norm(v3-v1);
-                cross_dir = cross(obj.normal,dir);
+                base = v3;
+                vec1 = v1-base;
+                vec2 = v2-base;
             else
                 base = v2;
-                dir = (v3-v2)/norm(v3-v2);
-                cross_dir = cross(obj.normal,dir);
+                vec1 = v3-base;
+                vec2 = v1-base;
             end
             
+            len = norm(vec1);
+            dir = (vec1)/len ;
+            
+%             cross_vec = vec2-dot(vec2,dir);
+            cross_dir1 = cross(obj.normal, vec1);
+            cross_dir = cross_dir1/norm(cross_dir1);
+            cross_len = sqrt(norm(vec2)^2 - dot(vec2,dir)^2);
+            cross_vec = cross_dir*cross_len;
+            
+            a= [0:point_spacing:len]';
+            linear_samples = [dir(1)*a,dir(2)*a,dir(3)*a]+repmat(base,length(a),1);
+                                    
+            numCross = ceil(cross_len/point_spacing);
+            all_samples = []; 
+            
+            for i = 0:numCross
                 
+                new_samples = linear_samples + repmat(cross_dir*point_spacing*i,size(linear_samples,1),1);
+                
+                all_samples = [all_samples;new_samples];
+            end            
+            
+%             figure, hold on
+% %             plot3([base(1),base(1)+len*dir(1)],[base(2),base(2)+len*dir(2)],[base(3),base(3)+len*dir(3)])
+% %             plot3(linear_samples(:,1),linear_samples(:,2),linear_samples(:,3),'.r')
+%             plot3(all_samples(:,1),all_samples(:,2),all_samples(:,3),'.k')
+%             V = [vert;vert(1,:)];
+%             plot3(V(:,1),V(:,2),V(:,3))
+%             axis equal
+                
+            v0 = repmat(vec1, size(all_samples,1),1);
+            v1 = repmat(vec2, size(all_samples,1),1);
+            v2 = all_samples - repmat(base, size(all_samples,1),1);
+            dot00 = dot(v0,v0,2);
+            dot01 = dot(v0,v1,2);
+            dot11 = dot(v1,v1,2);
+            dot02 = dot(v0,v2,2);
+            dot12 = dot(v1,v2,2);
+            
+            invDenom = 1./((dot00.*dot11) - (dot01.*dot01));
+            u = ((dot11.*dot02)-(dot01.*dot12)).*invDenom;
+            v = ((dot00.*dot12)-(dot01.*dot02)).*invDenom;
+            
+            key1 = u>-0.001;
+            key2 = v>-0.001;
+            key3 = (u+v)<1.0001;
+            
+            key = key1.*key2.*key3;
+            
+            points = deleteRowKey(all_samples,key);
+
+%             V = [vert;vert(1,:)];
+%             figure,hold on
+%             plot3(points(:,1),points(:,2),points(:,3),'r.')
+%             plot3(V(:,1),V(:,2),V(:,3))
+%             axis equal
+%             title('all constraints')
         end
     end
     
