4445b3be89f1f5870d8f3cce3bd94c1a38b2d103
ajahraus <adamjahraus@gmail.com>
2015-12-08 15:51:09 -0700

    Updates to Scratch

--- scratch.m
+++ scratch.m
@@ -5,6 +5,8 @@ clear all
 clc
 
 %% 
-I =  ImageClass([0,0,0],[0,0,0],Camera());
+I(1) =  ImageClass([0,0,0],[90,225,0],Camera());
+I(2) =  ImageClass([0,1,0],[90,315,0],Camera());
 
-I.displayImageInObjectSpace();
+I(1).displayImageInObjectSpace(1);
+I(2).displayImageInObjectSpace(1);
