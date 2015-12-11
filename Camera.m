260544375f291c1d21354324bc9b0ed66de9b5c7
ajahraus <adamjahraus@gmail.com>
2015-12-11 13:18:23 -0700

    Reverted Camera constructor

--- Camera.m
+++ Camera.m
@@ -10,13 +10,13 @@ classdef Camera
     
     methods
         function obj = Camera()
-            if nargin > 0
-                obj.principleDistance = 24; % mm
-                obj.principlePoint = [0,0]; % mm, not that it matters
-                obj.sensorArrayPx = [3264,4928]; % pixels, in rows then coloumns
-                obj.pixelSize = 0.00481; % mm, or 4.81 microns
-                obj.sensorSize = obj.sensorArrayPx *obj.pixelSize; % mm
-            end
+            
+            obj.principleDistance = 24; % mm
+            obj.principlePoint = [0,0]; % mm, not that it matters
+            obj.sensorArrayPx = [3264,4928]; % pixels, in rows then coloumns
+            obj.pixelSize = 0.00481; % mm, or 4.81 microns
+            obj.sensorSize = obj.sensorArrayPx *obj.pixelSize; % mm
+            
             
         end
         
