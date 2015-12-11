72b88f71a6bd8419dba59b3d821204293a8fcf0b
ajahraus <adamjahraus@gmail.com>
2015-12-11 12:58:07 -0700

    Minor Edits to Scratch

--- scratch.m
+++ scratch.m
@@ -5,14 +5,15 @@ clear all
 clc
 
 %% 
-I(1) =  ImageClass([0,0,0],[90,225,0],Camera());
+I(1) =  ImageClass([0,0,0],[90,225,0],Camera())
 I(2) =  ImageClass([0,1,0],[90,315,0],Camera());
+I(2)
 
 I(1).displayImageInObjectSpace(1);
 I(2).displayImageInObjectSpace(1);
 
-point = [-0.55,0.45,0; 1000,1000,1000;-0.55,0.45005,0;];
-plot3(point(1),point(2),point(3),'.')
+point = [-0.55,0.45,0; 0+sqrt(2),1+sqrt(2),0;-0.55,0.45005,0;];
+plot3(point(:,1),point(:,2),point(:,3),'.')
 axis equal
 
 %%
