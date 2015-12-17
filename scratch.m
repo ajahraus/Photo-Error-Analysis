53576e2333ea1191179fc93120d2dd87a6190099
ajahraus <adamjahraus@gmail.com>
2015-12-17 11:23:40 -0700

    Automating Camera positions

--- scratch.m
+++ scratch.m
@@ -5,14 +5,25 @@ clear
 clc
 
 %% 
-I(1) =  ImageClass([0,0,10],[0,0,0],Camera());
-I(2) =  ImageClass([0,1,10],[0,45,90],Camera());
+
+radInc =  [0: (360/60 *pi/180) : 2*pi]';
+camX = cos(radInc)*6;
+camY = sin(radInc)*6;
+camZ = ones(size(camX));
+camPos = [camX, camY+3,camZ];
+
+for i = 1:length(radInc)
+    I(i) =  ImageClass(camPos(i,:),[90,90+180/pi*atan2(camY(i),camX(i)),0],Camera());
+end
+
+% I(1) =  ImageClass([0,0,10],[0,0,0],Camera());
+% I(2) =  ImageClass([0,1,10],[0,15,90],Camera());
 
 
 planesModel = LoadPlanesFromFile('kuukpak planar model.txt');
 
 tic
-points = samplePlanesMulti(planesModel, 10000);
+points = samplePlanesMulti(planesModel, 5000);
 toc
 
 for i = 1:length(I)
@@ -25,15 +36,18 @@ end
 axis equal
 
 %%
+img_points = cell(length(I),1);
+for i = 1:length(I)
+    img_points{i} = I(i).observePoints(points);
+end
 
-img_points{1} = I(1).observePoints(points);
-img_points{2} = I(2).observePoints(points);
+%%
 
 for i = 1:length(img_points)
-    colour = rand(3,1);
     figure, hold on
     for j = 1:length(img_points{i})
-        plot(img_points{i}(j).coords(1,1),img_points{i}(j).coords(1,2),'.','Color',colour);
+        plot(img_points{i}(j).coords(1),img_points{i}(j).coords(2),'.');
     end
     axis equal
+    title(['Image ', num2str(i)]);
 end
