3d4f6360ee97dd1a9ebca71457c6a6bb0fb43fce
ajahraus <adamjahraus@gmail.com>
2015-12-17 10:37:15 -0700

    Changed Scratch to use new functions

--- scratch.m
+++ scratch.m
@@ -5,16 +5,19 @@ clear
 clc
 
 %% 
-I(1) =  ImageClass([0,0,0],[90,45,-45],Camera());
-I(2) =  ImageClass([0,1,0],[90,135,45],Camera());
-I(3) = ImageClass([0,0.5,0],[90,90,0],Camera());
+I(1) =  ImageClass([0,0,10],[0,0,0],Camera());
+I(2) =  ImageClass([0,1,10],[0,45,90],Camera());
 
-I(1).displayImageInObjectSpace(1);
-I(2).displayImageInObjectSpace(1);
-I(3).displayImageInObjectSpace(1);
 
-plane1 = Plane(eye(3)-repmat([1,0,0.5],3,1));
-points = plane1.samplePlane(100);
+planesModel = LoadPlanesFromFile('kuukpak planar model.txt');
+
+tic
+points = samplePlanesMulti(planesModel, 10000);
+toc
+
+for i = 1:length(I)
+    I(i).displayImageInObjectSpace(1);
+end
 
 for i = 1:length(points)
     plot3(points(i).xyz(1,1),points(i).xyz(1,2),points(i).xyz(1,3),'.')
@@ -25,14 +28,12 @@ axis equal
 
 img_points{1} = I(1).observePoints(points);
 img_points{2} = I(2).observePoints(points);
-img_points{3} = I(3).observePoints(points);
 
-% figure, hold on
 for i = 1:length(img_points)
     colour = rand(3,1);
     figure, hold on
     for j = 1:length(img_points{i})
         plot(img_points{i}(j).coords(1,1),img_points{i}(j).coords(1,2),'.','Color',colour);
-        axis equal
     end
+    axis equal
 end
