19d729e482be59e3a05235ad5be408152b042dc0
ajahraus <adamjahraus@gmail.com>
2016-01-06 11:05:40 -0700

    Improved Angle determination and cosmetic changes

--- scratch.m
+++ scratch.m
@@ -1,49 +1,70 @@
 % Photogrammetry Script
 
 close all
-clear 
+clear
 clc
 
-%% 
+tic
 
-radInc =  [0: (360/60 *pi/180) : 4*pi]';
-camX = cos(radInc)*6;
-camY = sin(radInc)*6;
+%%
+planesModel = LoadPlanesFromFile('kuukpak planar model.txt');
+points = samplePlanesMulti(planesModel, 5000);
+
+radInc = 360/20;
+rads =  [0: radInc : 360-radInc]';
+camX = cosd(rads)*7;
+camY = sind(rads)*7;
 camZ = ones(size(camX));
-camPos = [camX, camY+3,camZ];
+camPos = [camX, camY+3,camZ+1];
 
-for i = 1:length(radInc)
-    if i <= 61
-        I(i) =  ImageClass(camPos(i,:),[90,90+180/pi*atan2(camY(i),camX(i)),0],Camera());
-    else
-        I(i) =  ImageClass(camPos(i,:),[90,90+180/pi*atan2(camY(i),camX(i)),90],Camera());
+fig = 0;
+if fig
+    figure, hold on
+    for i = 1:length(points)
+        plot3(points(i).xyz(1,1),points(i).xyz(1,2),points(i).xyz(1,3),'.')
+    end
+    axis equal
+end
+
+angles = zeros(size(rads,1),3);
+for i = 1:length(angles)
+    Zvec = [camX(i), camY(i), camZ(i)+3]';
+    Zvec = Zvec/ sqrt(sum(Zvec.^2));
+    
+    Xvec = [-Zvec(2), Zvec(1), 0]';
+    Xvec = Xvec/ sqrt(sum(Xvec.^2));
+    
+    Yvec = cross(Zvec, Xvec);
+    
+    R = [Xvec, Yvec, Zvec]';
+    angles(i,:) = [atan2d(-R(3,2),R(3,3)),asind(R(3,1)),atan2d(-R(2,1), R(1,1))];
+    if fig
+        plot3(camPos(i,1),camPos(i,2),camPos(i,3),'.r')
+        quiver3(camPos(i,1),camPos(i,2),camPos(i,3),Zvec(1),Zvec(2),Zvec(3),0,'b')
+        quiver3(camPos(i,1),camPos(i,2),camPos(i,3),Xvec(1),Xvec(2),Xvec(3),0,'r')
+        quiver3(camPos(i,1),camPos(i,2),camPos(i,3),Yvec(1),Yvec(2),Yvec(3),0,'g')
     end
 end
 
-planesModel = LoadPlanesFromFile('kuukpak planar model.txt');
-
-tic
-points = samplePlanesMulti(planesModel, 5000);
 toc
+%%
 
-for i = 1:length(I)
-    I(i).displayImageInObjectSpace(1);
+for i = 1:length(rads)
+    I(i) =  ImageClass(camPos(i,:),angles(i,:),Camera());
 end
 
-for i = 1:length(points)
-    plot3(points(i).xyz(1,1),points(i).xyz(1,2),points(i).xyz(1,3),'.')
-end
-axis equal
-
+toc
 %%
 img_points = cell(length(I),1);
+
 for i = 1:length(I)
     img_points{i} = I(i).observePoints(points);
 end
 
+toc
 %%
 
-for i = 1:20:length(img_points)
+for i = 1:5:length(img_points)
     figure, hold on
     for j = 1:length(img_points{i})
         if ~isempty(img_points{i}(1).coords)
@@ -53,3 +74,5 @@ for i = 1:20:length(img_points)
     axis equal
     title(['Image ', num2str(i)]);
 end
+
+toc
