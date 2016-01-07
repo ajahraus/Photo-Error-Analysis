d3f18fad4e74b2abfe5a29ab528cea3045e3f37c
ajahraus <adamjahraus@gmail.com>
2016-01-07 11:37:56 -0700

    Small scale testing

--- scratch.m
+++ scratch.m
@@ -1,78 +1,29 @@
-% Photogrammetry Script
-
+clear all
 close all
-clear
 clc
 
-tic
-
 %%
-planesModel = LoadPlanesFromFile('kuukpak planar model.txt');
-points = samplePlanesMulti(planesModel, 5000);
+vertices = [0,0,0; 2,0,0; 0, 1, 0;];
+cur_plane = Plane(vertices);
 
-radInc = 360/20;
-rads =  [0: radInc : 360-radInc]';
-camX = cosd(rads)*7;
-camY = sind(rads)*7;
-camZ = ones(size(camX));
-camPos = [camX, camY+3,camZ+1];
+points_array = cur_plane.samplePlane(30);
 
-fig = 0;
-if fig
-    figure, hold on
-    for i = 1:length(points)
-        plot3(points(i).xyz(1,1),points(i).xyz(1,2),points(i).xyz(1,3),'.')
-    end
-    axis equal
-end
-
-angles = zeros(size(rads,1),3);
-for i = 1:length(angles)
-    Zvec = [camX(i), camY(i), camZ(i)+3]';
-    Zvec = Zvec/ sqrt(sum(Zvec.^2));
-    
-    Xvec = [-Zvec(2), Zvec(1), 0]';
-    Xvec = Xvec/ sqrt(sum(Xvec.^2));
-    
-    Yvec = cross(Zvec, Xvec);
-    
-    R = [Xvec, Yvec, Zvec]';
-    angles(i,:) = [atan2d(-R(3,2),R(3,3)),asind(R(3,1)),atan2d(-R(2,1), R(1,1))];
-    if fig
-        plot3(camPos(i,1),camPos(i,2),camPos(i,3),'.r')
-        quiver3(camPos(i,1),camPos(i,2),camPos(i,3),Zvec(1),Zvec(2),Zvec(3),0,'b')
-        quiver3(camPos(i,1),camPos(i,2),camPos(i,3),Xvec(1),Xvec(2),Xvec(3),0,'r')
-        quiver3(camPos(i,1),camPos(i,2),camPos(i,3),Yvec(1),Yvec(2),Yvec(3),0,'g')
-    end
-end
-
-toc
+I(1) =  ImageClass([1,-1,3],[25,0,0],Camera());
+I(2) = ImageClass([2,0.5,3],[0,25,0],Camera());
+I(3) = ImageClass([0,0.5,3],[0,-25,0],Camera());
 %%
-
-for i = 1:length(rads)
-    I(i) =  ImageClass(camPos(i,:),angles(i,:),Camera());
-end
-
-toc
-%%
-img_points = cell(length(I),1);
 
 for i = 1:length(I)
-    img_points{i} = I(i).observePoints(points);
-end
+    I(i).imageData = I(i).observePoints(points_array);
 
-toc
-%%
-
-for i = 1:5:length(img_points)
     figure, hold on
-    for j = 1:length(img_points{i})
-        if ~isempty(img_points{i}(1).coords)
-            plot(img_points{i}(j).coords(1),img_points{i}(j).coords(2),'.');
+    for j = 1:length(I(i).imageData)
+        if ~isempty(I(i).imageData(1).coords)
+            plot(I(i).imageData(j).coords(1),I(i).imageData(j).coords(2),'.');
         end
     end
     axis equal
-    title(['Image ', num2str(i)]);
+    set(gca,'Xlim',[-I(1).camera.sensorSize(1)/2, I(1).camera.sensorSize(1)/2])
+    set(gca,'Ylim',[-I(1).camera.sensorSize(2)/2, I(1).camera.sensorSize(2)/2])
+    title(['Image Number ',num2str(i)])
 end
-
-toc
