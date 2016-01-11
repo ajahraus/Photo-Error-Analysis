bc0f92b886ea9a263f2ba27088b8ac0613611e1e
ajahraus <adamjahraus@gmail.com>
2016-01-11 11:01:05 -0700

    Reduced points obs. Added comments

--- ImageClass.m
+++ ImageClass.m
@@ -93,47 +93,63 @@ classdef ImageClass
                 end
             end
             
-            FinalImageObs = obj.deleteNonUniqueImageCoords(imageObs);
+            FinalImageObs = obj.deleteNonUniqueImageCoords(imageObs, points);
         end
         
-        function FinalImagePoints = deleteNonUniqueImageCoords(obj, imageObs)
-            % TODO: reduce number of observations in the points array
+        function FinalImagePoints = deleteNonUniqueImageCoords(obj, imageObs, points)
+            
+            % declare point observation array and range to point arrays
             pnts = zeros(size(imageObs,2),2);
             rangeToPnt = zeros(length(imageObs),1);
             
+            % localize EOPs
             M = rotz(obj.direction(3))*roty(obj.direction(2))*rotx(obj.direction(1));
             X_0 = obj.location(1);
             Y_0 = obj.location(2);
             Z_0 = obj.location(3);
             
+            % check if image obs is empty
             if ~isempty(imageObs(1).point)
                 for i = 1:length(imageObs)
+                    % localize point locations
                     X = imageObs(i).point.xyz(1);
                     Y = imageObs(i).point.xyz(2);
                     Z = imageObs(i).point.xyz(3);
                     
                     localVec = M*[X-X_0;Y-Y_0;Z-Z_0];
                     
+                    % calculate range to point in local system
                     rangeToPnt(i) = abs(localVec(3));
                 end
                 
-                
+                % load image obs into local points array
                 for i = 1:size(imageObs,2)
                     pnts(i,:) = imageObs(i).coords;
                 end
                 
+                % round image obs to pixel coords (i.e. whole numbers)
                 roundedPnts = round(pnts/obj.camera.pixelSize);
+                % add index number to vector
                 rPntsIndex = [roundedPnts, (1:size(roundedPnts,1))'];
                 
+                % sorting image obs to find matches
                 testPoints = sortrows(rPntsIndex);
                 indexes = [];
                 
+                % loading indexes of points that match their previous row
+                % into vector of indexes 
                 for i = 2:length(roundedPnts)
                     if (testPoints(i-1,1) == testPoints(i,1))&(testPoints(i-1,2) == testPoints(i,2))
                         indexes = [indexes;testPoints(i-1,3),testPoints(i,3)];
                     end
                 end
                 
+                % Determining which of the matching points should be
+                % deletered. If the points come from the same plane, then
+                % the point with the id that comes first alphatetically
+                % should be used (to maximize redundancy). Otherwise, the
+                % point that is located closer to the camera should be
+                % used. 
                 indexesToSkip = [];
                 for i = 1:size(indexes,1)
                     if strcmp(imageObs(indexes(i,1)).point.planeName, imageObs(indexes(i,2)).point.planeName) & ~isempty(imageObs(indexes(i,1)).point.planeName)
@@ -148,16 +164,41 @@ classdef ImageClass
                         
                         if sum1 > sum2
                             indexesToSkip = [indexesToSkip,indexes(i,2)];
+                            
+                            % check all points in the points array. The one
+                            % which has a matching name to the point in the
+                            % imageObs class has its observation number
+                            % reduced by one. 
+                            for j = 1:length(points)
+                                if strcmp(points(j).pointName, imageObs(indexes(i,2)).point.pointName)
+                                    points(j).numObs = points(j).numObs - 1;
+                                end
+                            end
                         else
                             indexesToSkip = [indexesToSkip,indexes(i,1)];
+                            for j = 1:length(points)
+                                if strcmp(points(j).pointName, imageObs(indexes(i,1)).point.pointName)
+                                    points(j).numObs = points(j).numObs - 1;
+                                end
+                            end
                         end
                         
                     else
                         % Take the index with the lower range to the camera
                         if rangeToPnt(indexes(i,1)) > rangeToPnt(indexes(i,2))
                             indexesToSkip = [indexesToSkip,indexes(i,1)];
+                            for j = 1:length(points)
+                                if strcmp(points(j).pointName, imageObs(indexes(i,1)).point.pointName)
+                                    points(j).numObs = points(j).numObs - 1;
+                                end
+                            end
                         else
                             indexesToSkip = [indexesToSkip,indexes(i,2)];
+                            for j = 1:length(points)
+                                if strcmp(points(j).pointName, imageObs(indexes(i,2)).point.pointName)
+                                    points(j).numObs = points(j).numObs - 1;
+                                end
+                            end
                         end
                     end
                 end
